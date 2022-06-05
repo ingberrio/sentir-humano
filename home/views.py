79c10b829b4d0eae983e1dd54d1f28e78dc28350
Eduard Berrio <eduardberrio@MacBook-Pro-de-Eduard-2.local>
2022-06-04 20:16:11 -0500

    Function search field in Home

--- home/views.py
+++ home/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
 from suppliers.models import Supplier
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q 
 
 def home(request):
     suppliers = Supplier.objects.all()
-    number_suppliers = Supplier.objects.filter().count()
+    
+    # Search field home
+    
+    search_post = request.GET.get('search') # Get url parameter from a form submitting from a web page
+    if search_post:
+        suppliers = Supplier.objects.filter(Q(specialty__icontains=search_post) 
+                                          | Q(first_name__icontains=search_post)
+                                          | Q(city__icontains=search_post))
+    else:
+        # If not searched, return default posts
+        suppliers = Supplier.objects.all()
+
+    number_suppliers = Supplier.objects.filter().count() # Numbers of suppliers
+    
+    # Pagination 
+    
     page_num = request.GET.get('page', 1)
     paginator = Paginator(suppliers, 6) # 6 supplier per page
 
@@ -16,9 +32,11 @@ def home(request):
     except EmptyPage:
         # if the page is out of range, deliver the last page
         page_obj = paginator.page(paginator.num_pages)
+    
     context = {
         'suppliers': suppliers,
         'page_obj': page_obj,
         'number_suppliers': number_suppliers,
+        'search_post': search_post,
     }
     return render(request, 'home.html', context)
