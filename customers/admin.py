5a22b16ad78d7d2b823bf558043d17aab3032545
Eduard Berrio <eduardberrio@MacBook-Pro-de-Eduard-2.local>
2022-06-09 11:30:33 -0500

    Some fixes on Admin

--- customers/admin.py
+++ customers/admin.py
@@ -3,15 +3,16 @@ from .models import Customer, Appointment
 
 @admin.register(Appointment)
 class AppointmentAdmin(admin.ModelAdmin):
-    list_display = ["type_appointment", "customer"]
-    search_fields = ["type_appointment", "ecustomermail"]
-    exclude = ["password","last_login"]
+    list_display = ["added_by", "type_appointment", "customer", 'supplier', "start_date", "is_confirm", "is_cancel"]
+    search_fields = ['customer__first_name', 'customer__person_id']
+    list_filter = ["start_date", "supplier", "added_by" ]
+    exclude = ["password"]
     list_per_page = 10
 
 @admin.register(Customer)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ["first_name", "phone", "person_id", "is_active", "membership_id"]
-    search_fields = ["phone", "email"]
+    list_display = ["first_name", "phone", "person_id", "is_active", "membership_id", "person_id"]
+    search_fields = ["phone", "email", "person_id"]
     exclude = ["password","last_login"]
     list_filter = ["age", "is_active"]
     list_per_page = 10
--- customers/models.py
+++ customers/models.py
@@ -4,6 +4,7 @@ from memberships.models import Membership
 from suppliers.models import Supplier
 from users.models import NewUser
 from django.utils import timezone
+from django.conf import settings
 
 class Customer(AbstractBaseUser, models.Model):
     membership_id = models.ForeignKey(Membership, null=True, on_delete=models.SET_NULL, verbose_name='Membresia')
@@ -11,12 +12,12 @@ class Customer(AbstractBaseUser, models.Model):
     first_name = models.CharField("First name", max_length=255)
     last_name = models.CharField("Last name", max_length=255)
     person_id = models.CharField("Cedula", max_length=20, default=' ')
-    asesor_id = models.ForeignKey(NewUser, null=True, on_delete=models.SET_NULL, verbose_name='Asesor encargado', default='2')
-    email = models.EmailField()
-    age = models.CharField("Edad", max_length=3, default=' ')
-    city = models.CharField("Ciudad", max_length=20, default=' ')
-    neigbord = models.CharField("Barrio", max_length=20, default=' ')
-    phone = models.CharField("Telefono", max_length=20)
+    asesor_id = models.ForeignKey(NewUser, null=True, blank=True,  on_delete=models.SET_NULL, verbose_name='Asesor encargado', default='2')
+    email = models.EmailField(blank=True)
+    age = models.PositiveIntegerField("edad", blank=True)
+    city = models.CharField("Ciudad", blank=True, max_length=20, default=' ')
+    neigbord = models.CharField("Barrio", blank=True, max_length=20, default=' ')
+    phone = models.CharField("Telefono", blank=True, max_length=20)
     address =  models.TextField("Dirección", blank=True, null=True)
     description = models.TextField("Notas", blank=True, null=True)
     createdAt = models.DateTimeField("Created At", auto_now_add=True)
@@ -25,17 +26,34 @@ class Customer(AbstractBaseUser, models.Model):
     USERNAME_FIELD = 'email'
     
     def __str__(self):
-        return self.first_name
+        return self.person_id
 
 class Appointment(models.Model):
-    type_appointment = models.CharField("Tipo de cita", max_length=255)
+    
+    APPOIMENT_CHOICES = [
+        ('1', 'Tipo 1'),
+        ('2', 'Tipo 2'),
+        ('3', 'Tipo 3'),
+    ]
+
+    type_appointment = models.CharField("Tipo de cita", max_length=255, choices=APPOIMENT_CHOICES)
     customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL, verbose_name='Cliente')
+    affiliate = models.CharField("Afiliado", max_length=255, blank=True)
     supplier = models.ForeignKey(Supplier, null=True, on_delete=models.SET_NULL, verbose_name='Especialidad')
-    start_date = models.DateTimeField(default=timezone.now)
-    end_date = models.DateTimeField(null=True, blank=True)
+    start_date = models.DateTimeField("Inicio solicitud", default=timezone.now)
+    end_date = models.DateTimeField("Fecha finalizacion", null=True, blank=True)
+    added_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, verbose_name="Creado por")
     about = models.TextField("Observaciones", max_length=500, blank=True)
-    is_staff = models.BooleanField("Empleado", default=False)
-    is_active = models.BooleanField("Activo", default=False)
+    is_confirm = models.BooleanField("Confirmada", default=False)
+    is_cancel = models.BooleanField("Cancelada", default=False)
+
+    # Automatically set to current user to field Author when saved
+
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:
+            # Only set added_by during the first save.
+            obj.added_by = request.user
+        super().save_model(request, obj, form, change)
 
     def __str__(self):
         return self.type_appointment
