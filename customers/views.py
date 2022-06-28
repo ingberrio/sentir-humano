b85a95eebaa67394bd75016dd528211749ed8f0a
Eduard Berrio <eduardberrio@MacBook-Pro-de-Eduard-2.local>
2022-06-28 10:54:03 -0500

    Fixed render PDF 2.0

--- customers/views.py
+++ customers/views.py
@@ -1,13 +1,14 @@
-from django.views.generic import View
+from django.shortcuts import render
+from django.views.generic import CreateView, ListView, DeleteView, UpdateView, View
 from django.conf import settings
 import os
 from django.template.loader import get_template
 import customers
 from customers.models import Invoice
 from xhtml2pdf import pisa
-from django.http import HttpResponse, HttpResponseRedirect
-from django.urls import reverse
-from django.shortcuts import redirect
+from django.http import HttpResponse
+from django.http import JsonResponse, HttpResponseRedirect
+from django.urls import reverse_lazy
 
 
 class InvoicePdfView(View):
@@ -58,5 +59,4 @@ class InvoicePdfView(View):
             return response
         except:
             pass
-        
-        return redirect('admin/customers/invoice/')
+        return HttpResponseRedirect(reverse_lazy('admin:invoice'))
