b2482b770235cc81b2b33deb3282a7f0e4b662ea
Eduard Berrio <eduardberrio@MacBook-Pro-de-Eduard-2.local>
2022-06-29 15:05:58 -0500

    Add PDF function debug 4

--- customers/urls.py
+++ customers/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from customers.views import InvoicePdfView, CustomerPdfView
+from . import views
 
 app_name = 'customer'
 urlpatterns = [
-    path('admin/customers/customer/', CustomerPdfView.as_view(), name='credentialPDF'),
-    path('admin/customers/invoice/', InvoicePdfView.as_view(), name='generatePDF')
+    path('admin/customers/customer/', views.CustomerPdfView.as_view(), name='credentialPDF'),
+    path('admin/customers/invoice/', views.InvoicePdfView.as_view(), name='generatePDF')
 
 ]
