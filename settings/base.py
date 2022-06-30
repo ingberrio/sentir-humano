8c27abedf0000d14107cbc79a465e71f5a0ed833
Eduard Berrio <eduardberrio@MacBook-Pro-de-Eduard-2.local>
2022-06-30 17:10:00 -0500

    Pdf bug 6

--- settings/base.py
+++ settings/base.py
@@ -123,14 +123,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'users.newuser'
 AUTHENTICATION_BACKENDS = (
     'users.backends.CustomEmailAuthenticationBackend',
     'users.backends.CaseInsensitiveModelBackend',
     'django.contrib.auth.backends.AllowAllUsersModelBackend',
 )
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
