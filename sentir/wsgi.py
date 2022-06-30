df4e9acd0e53bf13bfcfa9c3bafaaf4d34e18601
Eduard Berrio <eduardberrio@MacBook-Pro-de-Eduard-2.local>
2022-06-30 11:59:49 -0500

    Add PDF function 4.8

--- sentir/wsgi.py
+++ sentir/wsgi.py
@@ -10,7 +10,6 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sentir.settings.dev')
