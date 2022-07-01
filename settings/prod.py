1e6a22149848217ecdee34f7e4fba7955ff5d717
Eduard Berrio <eduardberrio@MacBook-Pro-de-Eduard-2.local>
2022-07-01 17:44:17 -0500

    Settings Whitenoise 1

--- settings/prod.py
+++ settings/prod.py
@@ -35,7 +35,7 @@ AWS_HEADERS = {'Access-Control-Allow-Origin': '*'}
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 
