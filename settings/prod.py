8c46ff5e4c602d2a5412aeb7f8f4c400942ed72b
Eduard Berrio <eduardberrio@MacBook-Pro-de-Eduard-2.local>
2022-07-12 14:59:44 -0500

    Heroku deploy V2.4

--- settings/prod.py
+++ settings/prod.py
@@ -11,7 +11,7 @@ ALLOWED_HOSTS = [
     'https://sentir-humano.herokuapp.com/',
     'https://sentir-s3-bucket.s3.amazonaws.com/'
     ]
-CSRF_TRUSTED_ORIGINS = ['https://sentir-humano.herokuapp.com/']
+CSRF_TRUSTED_ORIGINS = ['https://*.herokuapp.com/', 'https://*.127.0.0.1']
 
 # Amazon S3 Settings
 
